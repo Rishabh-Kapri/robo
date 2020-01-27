f29710b4f1c0664c66fc9ac666897c2496b23f82
Rishabh-Kapri <rishabhkapri@gmail.com>
2020-01-27 14:06:31 +0530

    test changes on github robo

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
 		<div className='bg-green dib tc br3 pa3 ma2 grow bw2 shadow-5'>
 		  <img alt='' src={`https://robohash.org/test${id}?size=200x200`} />
 		  <div>
-		    <h2>{name}</h2>
+		    <h1>The github robo</h1>
 		    <p>{email}</p>
 		  </div>
 		</div>
